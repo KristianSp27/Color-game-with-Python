b4cc0f79fe39b9578483af787c6aa70f67b3bfed
kristian <kristian.spasov27@gmail.com>
2024-01-16 23:43:17 +0200

    Update

--- colourGame/main.py
+++ colourGame/main.py
@@ -6,3 +6,12 @@ CODE_LENGTH = 4
 
 def generate_code():
     code = []
+
+    for _ in range(CODE_LENGTH):
+        colour = random.choice(COLORS)
+    code.append(colour)
+
+    return code
+
+def guess_code():
+    guess = input("Guess: ").upper().split(" ")
